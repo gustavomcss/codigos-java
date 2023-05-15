1a0314498e61f55b4384ca0eb44cbe9fb294a614
Gustavo Corrêa <ecfgustavo@gmail.com>
2023-05-15 16:54:22 -0300

    Upload

---
+++ ListaExercicios/src/Ex001/ContaBancaria.java
@@ -0,0 +1,32 @@
+package Ex001;
+
+public class ContaBancaria {
+    private String nome;
+    private int numeroConta;
+    private double saldo;
+
+
+    public ContaBancaria(String nome, int numeroConta) {
+        this.nome = nome;
+        this.numeroConta = numeroConta;
+        this.saldo = 0.0;
+    }
+
+    public void depositar(double valor) {
+        saldo = saldo + valor;
+        System.out.println("Depósito Realizado!\n\nNovo Saldo: " + saldo);
+    }
+
+    public void sacar(double valor) {
+        if (valor <= saldo) {
+            saldo = saldo - valor;
+            System.out.println("Saque Realizado!\n\nNovo Saldo: " + saldo);
+        } else {
+            System.out.println("Saldo Insuficiente.");
+        }
+    }
+
+    public void verificarSaldo() {
+        System.out.println("Saldo Atual: " + saldo);
+    }
+}
---
+++ ListaExercicios/src/Ex001/Principal.java
@@ -0,0 +1,62 @@
+package Ex001;
+
+import java.util.Scanner;
+
+public class Principal {
+    
+    public static void exibirMenu() {
+        System.out.println("\n========= MENU =========\n");
+        System.out.println("[1] Depositar dinheiro");
+        System.out.println("[2] Sacar dinheiro");
+        System.out.println("[3] Verificar saldo");
+        System.out.println("[4] Sair do Programa");
+        System.out.print("\nEscolha uma opção: ");
+    }
+
+    public static void main(String[] args) {
+        Scanner s1 = new Scanner(System.in);
+        String nomeTitular;
+        int numeroConta, opc;
+        double valorDeposito, valorSaque;
+
+        System.out.print("Nome do Titutlar da Conta: ");
+        nomeTitular = s1.nextLine();
+        System.out.print("Digite o Numero da Conta: ");
+        numeroConta = s1.nextInt();
+
+        ContaBancaria conta = new ContaBancaria(nomeTitular, numeroConta);
+        System.out.println("\nBem Vindo, " + nomeTitular + "! O numero da sua conta eh: " + numeroConta);
+
+
+        do {
+            exibirMenu();
+            opc = s1.nextInt();
+
+            switch (opc) {
+                case 1:
+                    System.out.print("Digite o valor a ser depositado: ");
+                    valorDeposito = s1.nextDouble();
+                    conta.depositar(valorDeposito);
+                    break;
+                case 2:
+                    System.out.print("Digite o valor a ser sacado: ");
+                    valorSaque = s1.nextDouble();
+                    conta.sacar(valorSaque);
+                    break;
+                case 3:
+                    conta.verificarSaldo();
+                    break;
+                case 4:
+                    System.out.println("\nEncerrando Programa... ");
+                    break;
+                default:
+                    System.out.println("Opcao Invalida!!");
+                    break;
+            }
+
+        } while (opc != 4);
+        
+        s1.close();
+    }
+
+}
